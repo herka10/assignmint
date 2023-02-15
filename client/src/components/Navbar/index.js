3bab4106b731e46f9c94d07871c5c074def6820b
Pharos19 <Pharos199@gmail.com>
2023-02-15 15:17:09 -0700

    adding hover/collapsable clicks

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -102,6 +102,41 @@ const ResponsiveAppBar = () => {
                 <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
                     <IconButton
                         size='large'
+                        aria-label='account of current user'
+                        aria-controls='menu-appbar'
+                        aria-haspopup='true'
+                        onClick={handleOpenNavMenu}
+                        color='inherit'
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Menu
+                        id='menu-appbar'
+                        anchorEl={anchorElNav}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                        }}
+                        keepMounted
+                        TransformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}
+                        open={Boolean(anchorElNav)}
+                        onClose={handleCloseNavMenu}
+                        sx={{
+                            display: {xs: 'block', md: 'none' },
+                        }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Link href={`/${page}`} underline='hover'>
+                                        <Button>{page}</Button>
+                                    </Link>
+                                </MenuItem>
+                            ))}
+                            {showCollapseableSignin()}
+                        </Menu>
                 </Box>
             </Toolbar>
             </Container> 
