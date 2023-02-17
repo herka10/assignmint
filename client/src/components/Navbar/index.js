74e67ca6c7405179ffaae5da3b99b5701c308544
Pharos19 <Pharos199@gmail.com>
2023-02-16 18:33:05 -0700

    making changes in navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -58,3 +58,65 @@ const ResponsiveAppBar = () => {
         }
     };
 }
+
+return (
+    <AppBar position='static'>
+        <Container maxWidth='x1'>
+            <Toolbar disableGutters>
+                {/* collapsable menu */}
+                <HouseIcon sx={{ display: { xs: 'none', md: 'flex'}, mr: 1 }} />
+                <Typography
+                variant='h6'
+                noWrap
+                component='a'
+                href='/'
+                sx={{
+                    mr: 2
+                    display: {xs: 'none', md: 'flex' },
+                    fontFamily: 'monospace',
+                    fontWeight: 700,
+                    letterSpacing: '.3rem',
+                    color: 'inherit',
+                    textDecoration: 'none',
+                }}
+                >Assignmint
+                </Typography>
+
+                <Box sx={{ flexGrow: 1, display: {xs: 'flex', md: 'none'} }}>
+                    <IconButton
+                    size='large'
+                    aria-label='account of current user'
+                    aria-controls='menu-appbar'
+                    aria-haspopup='true'
+                    onClick={handleOpenNavMenu}
+                    color='inherit'
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Menu
+                id='menu-appbar'
+                anchorEl={anchoeElNav}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                    display: { xs: 'block', md: 'none' },
+                }}
+                >
+                    {pages.map((page) => (
+                        <MenuItem key={page} onClick={handleCloseNavMenu}>
+                            <Link href={`/${page}`} underline='hover'>
+                                <Button>{page}</Button>
+                            </Link>
+                        </MenuItem>
+                    ))}
+                    {showCollapseableSignin()}
+                </Menu>
+                </Box>
+            </Toolbar>
+        </Container>
+    </AppBar>
+)
