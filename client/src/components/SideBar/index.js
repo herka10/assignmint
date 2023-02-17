4e29675102011eddbeac52d09190ee04fca604d1
Pharos19 <Pharos199@gmail.com>
2023-02-16 20:47:47 -0700

    fixing sidebar

--- client/src/components/SideBar/index.js
+++ client/src/components/SideBar/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
+// import 'bootstrap-icons/font/bootstrap-icons.css'
 
 function SlideBar() {
     return(
