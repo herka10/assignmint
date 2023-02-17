2095523b72210d48146b02f0a1a5297a39c72c9e
James <andritjm@gmail.com>
2023-02-16 21:37:18 -0600

    finished mutations? seemingly?

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -60,7 +60,7 @@ export const REMOVE_USER = gql`
 `
 
 export const ADD_GROUP = gql`
-    mutation addGroup($title: String!, name: String, users: String!) {
+    mutation addGroup($title: String!, $name: String, $users: String!) {
         addGroup(title: $title, name: $name, users: $users) {
             token
             group {
@@ -72,7 +72,7 @@ export const ADD_GROUP = gql`
 `
 
 export const UPDATE_GROUP = gql`
-    mutation updateGroup($title: String!, name: String, users: [ID]) {
+    mutation updateGroup($title: String!, $name: String, $users: [ID]) {
         updateGroup(title: $title, name: $name, users: $users) {
             token
             group {
@@ -84,7 +84,7 @@ export const UPDATE_GROUP = gql`
 `
 
 export const REMOVE_GROUP = gql`
-    mutation removeGroup($title: String!, name: String, users: [ID]) {
+    mutation removeGroup($title: String!, $name: String, $users: [ID]) {
         removeGroup(title: $title, name: $name, users: $users) {
             token
             group {
@@ -96,7 +96,7 @@ export const REMOVE_GROUP = gql`
 `
 
 export const ADD_EVENT = gql`
-    mutation addEvent($title: String!, name: String, startDate: String!, dueDate: String!) {
+    mutation addEvent($title: String!, $name: String, $startDate: String!, $dueDate: String!) {
         addEvent(title: $title, name: $name, startDate: $startDate, dueDate: $dueDate) {
             token
             event {
@@ -108,7 +108,7 @@ export const ADD_EVENT = gql`
 `
 
 export const UPDATE_EVENT = gql`
-    mutation updateEvent($title: String!, name: String, startDate: String!, dueDate: String!) {
+    mutation updateEvent($title: String!, $name: String, $startDate: String!, $dueDate: String!) {
         updateEvent(title: $title, name: $name, startDate: $startDate, dueDate: $dueDate) {
             token
             event {
@@ -119,3 +119,74 @@ export const UPDATE_EVENT = gql`
     }
 `
 
+export const DELETE_EVENT = gql`
+    mutation deleteEvent($title: String!, $name: String, $startDate: String!, $dueDate: String!) {
+        deleteEvent(title: $title, name: $name, startDate: $startDate, dueDate: $dueDate) {
+            token
+            event {
+                _id
+                name
+            }
+        }
+    }
+`
+
+export const ADD_LIST = gql`
+    mutation addList($listName: String!, $createAt: string) {
+        addList(listName: $listName, createAt: $createAt) {
+            token
+            list {
+                _id
+                name
+            }
+        }
+    }
+`
+
+export const UPDATE_LIST = gql`
+    mutation updateList($listName: String!, $createAt: string) {
+        updateList(listName: $listName, createAt: $createAt) {
+            token
+            list {
+                _id
+                name
+            }
+        }
+    }
+`
+
+export const REMOVE_LIST = gql`
+    mutation removeList($listName: String!, $createAt: string) {
+        removeList(listName: $listName, createAt: $createAt) {
+            token
+            list {
+                _id
+                name
+            }
+        }
+    }
+`
+
+export const ADD_ITEM = gql`
+    mutation addItem($itemDescription: String, $quantity: Int) {
+        addItem(itemDescription: $itemDescription, quantity: $quantity) {
+            token
+            item {
+                _id
+                name
+            }
+        }
+    }
+`
+
+export const REMOVE_ITEM = gql`
+    mutation removeItem($itemDescription: String, $quantity: Int) {
+        removeItem(itemDescription: $itemDescription, quantity: $quantity) {
+            token
+            item {
+                _id
+                name
+            }
+        }
+    }
+`
